d40392dd5b30a28d46dfdc5d1a0f148f62d56c5d
vhfghcvnmb <dipetriantonio@hotmail.com>
2017-03-13 05:56:55 +0100

    Update Anagrams.java

--- src/com/toy/anagrams/ui/Anagrams.java
+++ src/com/toy/anagrams/ui/Anagrams.java
@@ -189,7 +189,7 @@ public class Anagrams extends JFrame {
         buttonsPanel.add(guessButton, gridBagConstraints);
 
         nextTrial.setMnemonic('N');
-        nextTrial.setText("New Word");
+        nextTrial.setText("Nytt ord");
         nextTrial.setToolTipText("Fetch a new word.");
         nextTrial.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
